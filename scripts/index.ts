632e76a421cd499e0ca208e8631e93ffc7bfea8a
Bowen <mr_lbw@outlook.com>
2018-07-10 22:38:14 +0800

    feat: complete deploy module

--- scripts/index.ts
+++ scripts/index.ts
@@ -4,6 +4,7 @@ const zlib = require('zlib')
 const { genMenu, contentList } = require('./generator')
 
 const { send } = micro
+const { stringify } = JSON
 const cwd: string = resolve(__dirname, '../')
 const catalogOutput: string = resolve(__dirname, '../menu.json')
 
@@ -15,18 +16,24 @@ const server = micro(async function (req: req, res: res) {
   res.setHeader('Content-Type', 'application/json; charset=utf-8')
   res.setHeader('Content-Encoding', 'gzip')
 
-  let path = req.url.slice(1) // delete `/` character in the beginning
-  console.log(`Request address is: /${path}`)
+  const url = req.url
+  console.log(`Request address is: ${url}`)
+
+  if (url === '/') return send(res, 200, zlib.gzipSync(stringify({
+    date: new Date()
+  })))
+
+  let path = url.slice(1) // delete `/` character in the beginning
   if (!contentList[path]) {
     return send(res, 404, zlib.gzipSync('File not found'))
   }
 
-  const data = JSON.stringify(contentList[path])
+  const data = stringify(contentList[path])
   send(res, 200, zlib.gzipSync(data))
 })
 
 genMenu(cwd, catalogOutput).then(() => {
-  const port = process.env.PORT || 8899
+  const port = process.env.PORT || 8800
   server.listen(port)
   console.info(`\n Server listening on http://localhost:${port} \n`);
 })
