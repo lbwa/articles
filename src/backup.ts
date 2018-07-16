d80226fa3c52a711a291b63e60dc4ebe17677c86
Bowen <mr_lbw@outlook.com>
2018-07-16 22:22:32 +0800

    feat: complete backup server module

--- src/backup.ts
+++ src/backup.ts
@@ -6,20 +6,40 @@ const { genMenu, contentList } = require('./generator')
 
 const { send } = micro
 const { stringify } = JSON
+
+const whiteList = [
+  'https://lbwa.github.io',
+  'https://set.sh'
+]
+const HOST = process.env.HOST || '127.0.0.1' // Should be from pm2
+const PORT = process.env.PORT || 8800 // same as HOST variable
+
+const isDev = process.env.NODE_ENV === 'development'
 const cwd: string = resolve(__dirname, '../')
 const catalogOutput: string = resolve(__dirname, '../menu.json')
 
 import { ServerResponse, ServerRequest } from 'http'
 
 const server = micro(async function (req: ServerRequest, res: ServerResponse) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  res.setHeader('Content-Type', 'application/json; charset=utf-8')
-  res.setHeader('Content-Encoding', 'gzip')
-
   const url = req.url
   console.log(`Request address is: ${url}`)
 
+  let origin: string
+
+  if (isDev) {
+    origin = '*'
+  } else {
+    const index = whiteList.indexOf(`${req.headers.origin}`)
+    origin = index  > -1 ? whiteList[index] : 'https://set.sh'
+  }
+
+  res.setHeader('Access-Control-Allow-Origin', `${origin}`)
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST')
+  res.setHeader('Vary', 'Origin, Accept-Encoding')
+  res.setHeader('Content-Type', 'application/json; charset=utf-8')
+  res.setHeader('Content-Encoding', 'gzip')
+
+  // ! detect source IP
   // const origin = req.headers['x-forwarded-for']
   //   || req.connection.remoteAddress
   //   || req.socket.remoteAddress
@@ -31,13 +51,13 @@ const server = micro(async function (req: ServerRequest, res: ServerResponse) {
     })))
   }
 
-  if (url === '/menu.json') {
+  if (url === '/menu') {
     return send(res, 200, zlib.gzipSync(
       fs.readFileSync(resolve(__dirname, '../menu.json'), 'utf8')
     ))
   }
 
-  if (url === '/project.json') {
+  if (url === '/project') {
     return send(res, 200, zlib.gzipSync(
       fs.readFileSync(resolve(__dirname, '../projects/projects.json'), 'utf8')
     ))
@@ -57,7 +77,6 @@ genMenu(cwd, catalogOutput).then(() => {
   // 主要用于部署前生成 menu.json
   if (process.argv[2] === 'skip') return
 
-  const port = process.env.PORT || 8800
-  server.listen(port)
-  console.info(`\n Server listening on http://localhost:${port} \n`)
+  server.listen(PORT)
+  console.info(`\n Server listening on http://${HOST}:${PORT} \n`)
 })
