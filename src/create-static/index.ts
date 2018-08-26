e948b538811013df2c01b84136a5772755947f0f
Bowen <mr_lbw@outlook.com>
2018-08-26 11:59:10 +0800

    feat: support static mode

--- src/create-static/index.ts
+++
@@ -1,6 +0,0 @@
-const CreateStatic = require('./core')
-const path = require('path')
-
-new CreateStatic({
-  cwd: path.resolve(process.cwd(), './')
-})
---
+++ src/static.ts
@@ -0,0 +1,12 @@
+const DocsServer = require('docs-server')
+
+new DocsServer({
+  mode: 'static',
+  staticNormalize (origin: string): string {
+    const removeShortDate = origin.replace(/\/{0}(\d{6}-)+/g, '')
+    const removeInitialYear = removeShortDate.replace(/^\d{4}/, '')
+    const removeRepeat = removeInitialYear.replace(/^\/\S+\//, '')
+    const removeExtension = removeRepeat.replace(/\.md$/, '')
+    return `writings/${removeExtension}`
+  }
+})
